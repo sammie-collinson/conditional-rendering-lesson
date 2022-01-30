1f0009532dd4752ae53356cc10d6006bf3231dc2
Sammie Collinson <heroldsamantha@gmail.com>
2022-01-30 09:35:39 -0500

    added Login and Logout button components, imported into App.js, added useState and handling functions to toggle state.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import React, {useState} from 'react';
 import Greeting from './components/Greeting';
+import LogoutButton from './components/LogoutButton';
+import LoginButton from './components/LoginButton';
 
 const App = () => {
+
+  const [isLoggedIn, toggleLogin] = useState(false);
+
+  const handleLoginClick = () => toggleLogin(true);
+  const handleLogoutClick = () => toggleLogin(false);
+
+  let button;
+  if(isLoggedIn) {
+    button = <LogoutButton onClick={handleLogoutClick}/>
+  } else {
+    button = <LoginButton onClick={handleLoginClick}/>
+  }
+
   return (
     <div className="App">
-      <Greeting isLoggedIn={false} />
+      <Greeting isLoggedIn={isLoggedIn} />
+      {button}
     </div>
   );
 }
